b035c33927628839d6794a8f9d15b91c7c938a52
vaibhad2103 <bountydange@gmail.com>
2022-04-26 21:45:00 +0530

    [UPDATE] Header component working

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,45 +3,55 @@ import { styles } from "../styles";
 import NavBar from "./NavBar";
 
 function Header() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100vw",
-        marginTop: 40,
-      }}
-    >
-      <NavBar />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <img
-          src={require("../assets/profile.jpeg")}
-          style={styles.profileImage}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            marginLeft: 10,
-          }}
-        >
-          <a style={styles.name}>
-            <span style={{ opacity: 0.5 }}>Hey! I'm</span> Vaibhav Dange
-          </a>
-          <a style={styles.bio}>
-            Computer engineering student, software developer and a sports
-            enthusiast...
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				width: "100vw",
+				marginTop: 40,
+			}}
+		>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+				}}
+			>
+				<img
+					src={require("../assets/profile.jpeg")}
+					style={styles.profileImage}
+				/>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						marginLeft: 10,
+					}}
+				>
+					<a style={styles.name}>
+						<span style={{ color: "#89a" }}>
+							Hey!{" "}
+							<img
+								src={require("../assets/handwave.png")}
+								className="hello_image"
+							/>{" "}
+							<br />
+							I'm
+						</span>{" "}
+						<span style={{ textShadow: "2px 4px 5px #000" }}>
+							Vaibhav Dange
+						</span>
+					</a>
+					<a style={styles.bio}>
+						Computer engineering student, software developer and a sports
+						enthusiast...
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -43,61 +43,15 @@ function Landing() {
 				width,
 			}}
 		>
-			{/* <Header /> */}
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					width: "100vw",
-					marginTop: 40,
-				}}
-			>
-				<NavBar
-					color={window.scrollY > 1000 ? "rgba(0,0,0,0.3)" : "rgba(0,0,0,0.8)"}
-					eduClick={() => eduRef.current.scrollIntoView()}
-					skillClick={() => skillRef.current.scrollIntoView()}
-					hobClick={() => hobRef.current.scrollIntoView()}
-					proClick={() => proRef.current.scrollIntoView()}
-				/>
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-					}}
-				>
-					<img
-						src={require("../assets/profile.jpeg")}
-						style={styles.profileImage}
-					/>
-					<div
-						style={{
-							display: "flex",
-							flexDirection: "column",
-							marginLeft: 10,
-						}}
-					>
-						<a style={styles.name}>
-							<span style={{ color: "#89a" }}>
-								Hey!{" "}
-								<img
-									src={require("../assets/handwave.png")}
-									className="hello_image"
-								/>{" "}
-								<br />
-								I'm
-							</span>{" "}
-							<span style={{ textShadow: "2px 4px 5px #000" }}>
-								Vaibhav Dange
-							</span>
-						</a>
-						<a style={styles.bio}>
-							Computer engineering student, software developer and a sports
-							enthusiast...
-						</a>
-					</div>
-				</div>
-			</div>
+			<NavBar
+				color={window.scrollY > 1000 ? "rgba(0,0,0,0.3)" : "rgba(0,0,0,0.8)"}
+				eduClick={() => eduRef.current.scrollIntoView()}
+				skillClick={() => skillRef.current.scrollIntoView()}
+				hobClick={() => hobRef.current.scrollIntoView()}
+				proClick={() => proRef.current.scrollIntoView()}
+			/>
+			<Header />
+
 			<div ref={eduRef}>
 				<Education />
 			</div>
